d0faf3108dd36c5cd7d192d092a69823f8e51d0f
Diego Cullanco <dieg@mail.com>
2025-07-02 12:15:53 -0500

    Gestionar categorias y productos

---
+++ Admin/EliminarProducto.php
@@ -0,0 +1,19 @@
+<?php
+include_once "../includes/conector.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST["product-id"]; // Asegúrate de que el nombre del campo coincide
+
+    $obj = new Conectar();
+    $sql = "DELETE FROM producto WHERE IdProducto = ?";
+    $stmt = $obj->getConexion()->prepare($sql);
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        header("Location: Productos.php");
+        exit();
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+}
+?>
