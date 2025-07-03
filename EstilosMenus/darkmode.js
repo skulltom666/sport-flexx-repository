0bb6593c7770fb2bffecce2d0979b8fa55490788
skulltom666 <skulltom666@outlook.com>
2025-07-02 23:27:49 -0500

    areglo de carpetas

--- EstilosMenus/darkmode.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function () {
-    const theme = localStorage.getItem('theme');
-    if (theme === 'dark') {
-        document.body.classList.add('dark-mode');
-        document.getElementById("darkModeIcon").classList.remove("fa-moon");
-        document.getElementById("darkModeIcon").classList.add("fa-sun");
-    }
-};
